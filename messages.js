9a486470570965c1c09ca3877cad2366f1094047
SakiSakiSakiSakiSaki <chowdhurysakin@gmail.com>
2019-12-22 23:05:07 -0500

    Changed bold abbreviations to message box format

--- messages.js
+++ messages.js
@@ -1,8 +1,8 @@
 module.exports = {
   // TODO: Append the image
-  firstGreeting: `Welcome to the Baruch College Server! 
+  firstGreeting: `**Welcome to the Baruch College Server! 
 If you'd like, you can mention your major for a role. 
-Feel free to ask any questions.
+Feel free to ask any questions.**
 
 **RULES:**
 **1)** No bullying or harassment. Be respectful to one another. 
@@ -13,34 +13,34 @@ Feel free to ask any questions.
 **6)** No NSFW content of ANY KIND OR FORM.
 **7)** No solicitation of illegal substances.`,
   askMajor: `If you'd like to receive a role you can respond with your (current or intended) major:
-  "**ACC**" for Accounting
-  "**ACT**" for Actuarial Science
-  "**ART**" for Arts Administration
-  "**AAAS**" for Asian and Asian American Studies
-  "**BCOM**" for Business Communications
-  "**BIO**" for Biological Sciences 
-  "**CIS**" for Computer Information Sytems
-  "**COMS**" for Communication Studies
-  "**ECO**" for Economics
-  "**ENG**" for English
-  "**FIN**" for Finance
-  "**FINM**" for Financial Mathematics
-  "**IOP**" for Industrial/Organizational Psychology
-  "**INTB**" for International Business
-  "**JRN**" for Journalism
-  "**MGT**" for Management
-  "**MKT**" for Marketing
-  "**MTH**" for Math
-  "**PHI**" for Philosophy
-  "**PHY**" for Physics
-  "**POL**" for Political Science
-  "**PSY**" for Psychology
-  "**PAF**" for Public Affairs
-  "**QMM**" for Quantitative Methods and Modeling
-  "**RES**" for Real Estate
-  "**SQM**" for Statistics and Quantitative Modeling
-  "**SOC**" for Sociology
-  "**STA**" for Statistics
-  "**Undecided**" for Major Undecided
+  "`ACC`" for Accounting
+  "`ACT`" for Actuarial Science
+  "`ART`" for Arts Administration
+  "`AAAS`" for Asian and Asian American Studies
+  "`BCOM`" for Business Communications
+  "`BIO`" for Biological Sciences 
+  "`CIS`" for Computer Information Sytems
+  "`COMS`" for Communication Studies
+  "`ECO`" for Economics
+  "`ENG`" for English
+  "`FIN`" for Finance
+  "`FINM`" for Financial Mathematics
+  "`IOP`" for Industrial/Organizational Psychology
+  "`INTB`" for International Business
+  "`JRN`" for Journalism
+  "`MGT`" for Management
+  "`MKT`" for Marketing
+  "`MTH`" for Math
+  "`PHI`" for Philosophy
+  "`PHY`" for Physics
+  "`POL`" for Political Science
+  "`PSY`" for Psychology
+  "`PAF`" for Public Affairs
+  "`QMM`" for Quantitative Methods and Modeling
+  "`RES`" for Real Estate
+  "`SQM`" for Statistics and Quantitative Modeling
+  "`SOC`" for Sociology
+  "`STA`" for Statistics
+  "`Undecided`" for Major Undecided
 `
 }
