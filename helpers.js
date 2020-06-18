8942cea06d8d591a22e1bcb7857c100dd8339e22
FrankMerin <61675381+FrankMerin@users.noreply.github.com>
2020-06-18 01:42:01 -0400

    Reddit Posting Feature Complete

--- helpers.js
+++ helpers.js
@@ -55,4 +55,9 @@ module.exports = {
       return client.users.get(mention);
     }
   },
+  isRedditPostingTime: () => {
+    // If the time is: 6am, 2pm, 10pm, return true. Else return false.
+    const time = new Date().getHours();
+    return time === 6 || time === 14 || time === 22
+  },
 };
